5f3d292d4e4a9ede592129658f6e97c7833098ab
rishitha_garikipati <2200030883@kluniversity.in>
2023-12-27 17:13:12 +0530

    Navbar using bootstrap

--- traveltourismmanagement/sampleapp/urls.py
+++ traveltourismmanagement/sampleapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("",views.home),
+    path("home",views.home),
     path("addStudent",views.addStudent,name="addStudent"),
-    path("insertStudent",views.insertStudent,name="insertStudent")
+    path("insertStudent",views.insertStudent,name="insertStudent"),
+    path("contact",views.contact,name="contact"),
 ]
+
+
+
+
--- traveltourismmanagement/sampleapp/views.py
+++ traveltourismmanagement/sampleapp/views.py
@@ -2,17 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Student
 # Create your views here.
-
 def home(request):
     return render(request,'home.html')
-
 def addStudent(request):
     return render(request,"addStudent.html")
-
 def insertStudent(request):
     sid=int(request.POST['sid'])
     sname=request.POST['sname']
     st=Student(sid=sid,sname=sname)
     Student.save(st)
-
     return render(request,'display.html')
+def contact(request):
+    return render(request,"contact.html")
