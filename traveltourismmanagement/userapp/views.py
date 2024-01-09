d8d5742d6b1e2c3c2edf6d9db9bfd5edf553e378
rishitha_garikipati <2200030883@kluniversity.in>
2024-01-09 12:41:21 +0530

    Navbar and background image
    Signed-off-by: rishitha_garikipati <2200030883@kluniversity.in>

--- traveltourismmanagement/userapp/views.py
+++ traveltourismmanagement/userapp/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"base.html")
+    return  render(request,"base.html")
