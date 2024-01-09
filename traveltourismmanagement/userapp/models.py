633966607889f2cbde70adb7b82eb62a1308d2db
rishitha_garikipati <2200030883@kluniversity.in>
2024-01-09 11:24:29 +0530

    creation of admin,general and userapps and displaying the title in center
    Signed-off-by: rishitha_garikipati <2200030883@kluniversity.in>

--- traveltourismmanagement/userapp/models.py
+++ traveltourismmanagement/userapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Roles(models.Model):
+    role_id = models.IntegerField(blank=False, primary_key=True)
+    role_type = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        db_table = 'roles'
+
+class Users(models.Model):
+    user_id = models.IntegerField(blank=False, primary_key=True)
+    username = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    password =models.CharField(max_length=100, blank=False)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+    is_active = models.BooleanField(max_length=100, blank=False)
+    roles = models.ForeignKey(Roles,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'users'
