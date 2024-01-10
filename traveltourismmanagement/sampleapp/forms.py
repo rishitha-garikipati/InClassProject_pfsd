7bcd4353f3e77dd8083e65e46909ff9347870f7f
rishitha_garikipati <2200030883@kluniversity.in>
2024-01-10 16:47:30 +0530

    userapp created
    Signed-off-by: rishitha_garikipati <2200030883@kluniversity.in>

--- traveltourismmanagement/sampleapp/forms.py
+++ traveltourismmanagement/sampleapp/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class EmployeeForm(forms.Form):
     emp_id = forms.IntegerField(label="Enter Employee ID:")
-    emp_name=forms.CharField(label="Enter Employee Name:",max_lenght=50)
-    emp_desig = forms.CharField(label="Enter Employee Designation:", max_lenght=50)
+    emp_name=forms.CharField(label="Enter Employee Name:",max_length=50)
+    emp_desig = forms.CharField(label="Enter Employee Designation:", max_length=50)
