ad1f8d53a93d92fc2b97cebbfeee1a1823bd026e
rishitha_garikipati <2200030883@kluniversity.in>
2024-01-24 16:07:31 +0530

    userapp created
    Signed-off-by: rishitha_garikipati <2200030883@kluniversity.in>

--- traveltourismmanagement/userapp/urls.py
+++ traveltourismmanagement/userapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.home),
+    path("signup",views.signup,name="signup")
 ]
--- traveltourismmanagement/userapp/views.py
+++ traveltourismmanagement/userapp/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import UsersForm
+# Create your views here.
 def home(request):
-    return render(request,"base.html")
+    return  render(request,"base.html")
+
+def signup(request):
+    if request.method == "POST":
+        userform = UsersForm(request)
+    else:
+        userform = UsersForm()
+    return render(request,"signup.html",{"form":userform})
