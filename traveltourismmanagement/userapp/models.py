1ec9385ed02e93f6b08a4eb73ebb1e8dfda422ea
rishitha_garikipati <2200030883@kluniversity.in>
2024-02-13 11:51:30 +0530

    columns in users tables are edited

--- traveltourismmanagement/userapp/models.py
+++ traveltourismmanagement/userapp/models.py
@@ -6,15 +6,15 @@ from django.db import models
 # Create your models here.
 
 
+class User:
+    pass
+
+
 class Users(models.Model):
-    user_id = models.IntegerField(blank=False,primary_key=True,auto_created=True)
-    username = models.CharField(max_length=100,blank=False)
-    email = models.EmailField(max_length=100,blank=False)
-    password = models.CharField(max_length=100,blank=False)
-    first_name = models.CharField(max_length=100,blank=False)
-    last_name = models.CharField(max_length=100,blank=False)
-    is_active = models.BooleanField(max_length=10,blank=False)
-    role = models.ForeignKey(Roles,on_delete=models.CASCADE)
+    user_id = models.ForiegnKey(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_lenght=10)
+    gender = models.CharField(max_lenght=10)
+    img = models.ImageField(upload_to="")
 
     class Meta:
         db_table = "users"
